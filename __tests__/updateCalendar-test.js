c19e31b700b4731e35b6e56a2631eb084670bab2
girdhari.a <girdhari.a@zohocorp.com>
2019-07-13 23:05:19 +0530

    [INTERNAL] Added tests cases to test the functionality

--- __tests__/updateCalendar-test.js
+++ __tests__/updateCalendar-test.js
@@ -28,3 +28,49 @@ test('renders the birthday data on week basis', function() {
   expect(document.getElementById('day-6').textContent).toEqual('');
   expect(document.getElementById('day-0').textContent).toEqual('GWDDRLSB');
 });
+
+test('removes the previous data when computated again', function() {
+  // Set up our document body
+  document.body.innerHTML
+    = '<div id="day-1"></div><div id="day-2"></div><div id="day-3"></div><div id="day-4"></div><div id="day-5"></div><div id="day-6"></div><div id="day-0"></div>'
+    + '<textarea id="birthdayData" rows="20" cols="100" placeholder="Enter the given birthday data"></textarea>'
+    + '<input id="currentYear" type="number" placeholder="Enter the current year">';
+
+  var sampleData = fs.readFileSync(path.join(__dirname, '../sample/sample-input.txt')).toString();
+
+  document.getElementById('birthdayData').value = sampleData;
+  document.getElementById('currentYear').value = '2019';
+
+  eval(fs.readFileSync(path.join(__dirname, '../assets/js/index.js')) + '; updateCalendar(); updateCalendar();'); // computing the calendar data again
+
+  expect(document.getElementById('day-1').textContent).toEqual('GWFW');
+  expect(document.getElementById('day-2').textContent).toEqual('NLTR');
+  expect(document.getElementById('day-3').textContent).toEqual('HJPDMJPLJPSSAW');
+  expect(document.getElementById('day-4').textContent).toEqual('VWHGPW');
+  expect(document.getElementById('day-5').textContent).toEqual('RWMMRH');
+  expect(document.getElementById('day-6').textContent).toEqual('');
+  expect(document.getElementById('day-0').textContent).toEqual('GWDDRLSB');
+});
+
+test('returns empty data when current year is less than the birthdate', function() {
+  // Set up our document body
+  document.body.innerHTML
+    = '<div id="day-1"></div><div id="day-2"></div><div id="day-3"></div><div id="day-4"></div><div id="day-5"></div><div id="day-6"></div><div id="day-0"></div>'
+    + '<textarea id="birthdayData" rows="20" cols="100" placeholder="Enter the given birthday data"></textarea>'
+    + '<input id="currentYear" type="number" placeholder="Enter the current year">';
+
+  var sampleData = fs.readFileSync(path.join(__dirname, '../sample/sample-input.txt')).toString();
+
+  document.getElementById('birthdayData').value = sampleData;
+  document.getElementById('currentYear').value = '1900';
+
+  eval(fs.readFileSync(path.join(__dirname, '../assets/js/index.js')) + '; updateCalendar();');
+
+  expect(document.getElementById('day-1').textContent).toEqual('');
+  expect(document.getElementById('day-2').textContent).toEqual('');
+  expect(document.getElementById('day-3').textContent).toEqual('');
+  expect(document.getElementById('day-4').textContent).toEqual('');
+  expect(document.getElementById('day-5').textContent).toEqual('');
+  expect(document.getElementById('day-6').textContent).toEqual('');
+  expect(document.getElementById('day-0').textContent).toEqual('');
+});
